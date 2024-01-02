6641940099dceb8b5d0715949d3b9db0aaf89f15
Nikita <nikita.h@visualcomposer.com>
2024-01-02 22:50:29 +0200

    Add initial files

---
+++ includes/handlers/register-handler.php
@@ -0,0 +1,57 @@
+<?php
+
+function sanitizeFormUsername ($inputText) {
+  $inputText = strip_tags($inputText);
+  $inputText = str_replace(" ", "", $inputText);
+  return $inputText;
+}
+
+function sanitizeFormPassword ($inputText) {
+  $inputText = strip_tags($inputText);
+  return $inputText;
+}
+
+function sanitizeFormString ($inputText) {
+  $inputText = strip_tags($inputText);
+  $inputText = str_replace(" ", "", $inputText);
+  $inputText = ucfirst(strtolower($inputText));
+  return $inputText;
+}
+
+function validateUsername ($un) {
+  
+}
+
+function validateFirstname ($fn) {
+
+}
+
+function validateLastname ($ln) {
+
+}
+
+function validateEmails ($em, $em2) {
+
+}
+
+function validatePasswords ($pw, $pw2) {
+
+}
+
+if (isset($_POST['registerButton'])) {
+  $username = sanitizeFormUsername($_POST['username']);
+  $firstName = sanitizeFormString($_POST['firstName']);
+  $lastName = sanitizeFormString($_POST['lastName']);
+  $email = sanitizeFormString($_POST['email']);
+  $email2 = sanitizeFormString($_POST['email2']);
+  $password = sanitizeFormPassword($_POST['password']);
+  $password2 = sanitizeFormPassword($_POST['password2']);
+
+  validateUsername($username);
+  validateFirstame($firstName);
+  validateLastname($lastName);
+  validateEmails($email, $email2);
+  validatePasswords($password, $password2);
+
+}
+?>
---
+++ register.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<?php
+  include("includes/handlers/register-handler.php");
+  include("includes/handlers/login-handler.php");
+?>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Register page!</title>
+</head>
+<body>
+  <div id="inputContainer">
+    <!-- action is what page we're going to send the data to (in this case the current page) -->
+    <form action="register.php" method="POST" id="loginForm">
+      <h2>Login to your account</h2>
+      <p>
+        <label for="loginUsername">Username</label>
+        <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. John Doe" required>
+      </p>
+      <p>
+        <label for="loginPassword">Password</label>
+        <input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
+      </p>
+      <button type="submit" name="loginButton">Log in</button>
+    </form>
+
+    <form action="register.php" method="POST" id="registerForm">
+      <h2>Create your free account</h2>
+      <p>
+        <label for="username">Username</label>
+        <input id="username" name="username" type="text" placeholder="e.g. John Doe" required>
+      </p>
+
+      <p>
+        <label for="firstName">First name</label>
+        <input id="firstName" name="firstName" type="text" placeholder="John" required>
+      </p>
+
+      <p>
+        <label for="lastName">Last name</label>
+        <input id="lastName" name="lastName" type="text" placeholder="Doe" required>
+      </p>
+
+      <p>
+        <label for="email">Email</label>
+        <input id="email" name="email" type="email" placeholder="john@doe.com" required>
+      </p>
+      <p>
+        <label for="email2">Confirm email</label>
+        <input id="email2" name="email2" type="email" placeholder="john@doe.com" required>
+      </p>
+
+      <p>
+        <label for="password">Password</label>
+        <input id="password" name="password" type="password" placeholder="Your password" required>
+      </p>
+      <p>
+        <label for="password2">Confirm password</label>
+        <input id="password2" name="password2" type="password" placeholder="Your password" required>
+      </p>
+      <button type="submit" name="registerButton">Sign in</button>
+    </form>
+  </div>
+</body>
+</html>
