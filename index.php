7ac29db2094b639655f3e91e87c22d82c05bb153
Nikita <nikita.h@visualcomposer.com>
2024-01-08 22:31:23 +0200

    Section 6, Add navigation menu

--- index.php
+++ index.php
@@ -23,74 +23,107 @@
   </head>
   <body>
 
-    <div id="nowPlayingBarContainer">
-      <div id="nowPlayingBar">
+    <div id="mainContainer">
 
-        <div id="nowPlayingLeft">
-          <div class="content">
-            <span class="albumLink">
-              <img class="albumArtwork" src="https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2011/11/square-format-01.jpg" alt="">
-            </span>
-            <div class="trackInfo">
-              <span class="trackName">
-                <span>Happy Birthday</span>
-              </span>
-              <span class="artistName">
-                <span>John Doe</span>
-              </span>
-            </div>
-          </div>
-        </div>
+      <div id="topContainer">
+        <div id="navBarContainer">
+          <nav class="navBar">
+            <a href="index.php" class="logo">
+              <i class="fa-solid fa-shield-cat"></i>
+            </a>
 
-        <div id="nowPlayingCenter">
-          <div class="content playerControls">
-            <div class="buttons">
-              <button class="controlButton shuffle" title="Shuffle">
-                <i class="fa-solid fa-shuffle"></i>
-              </button>
-              <button class="controlButton previous" title="Previous">
-                <i class="fa-solid fa-arrow-left"></i>
-              </button>
-              <button class="controlButton play" title="Play">
-                <i class="fa-solid fa-play"></i>
-              </button>
-              <button class="controlButton pause" title="Pause" style="display:none">
-                <i class="fa-solid fa-pause"></i>
-              </button>
-              <button class="controlButton next" title="Next">
-                <i class="fa-solid fa-arrow-right"></i>
-              </button>
-              <button class="controlButton repeat" title="Repeat">
-                <i class="fa-solid fa-repeat"></i>
-              </button>
-            </div>
-
-            <div class="playbackBar">
-              <span class="progressTime current">0:00</span>
-              <div class="progressBar">
-                <div class="progressBarBg">
-                  <div class="progress"></div>
-                </div>
+            <div class="group">
+              <div class="navItem">
+                <a href="search.php" class="navItemLink">Search <i class="fa-solid fa-magnifying-glass searchIcon"></i></a>
               </div>
-              <span class="progressTime remaining">0:00</span>
             </div>
-          </div>
-        </div>
 
-        <div id="nowPlayingRight">
-          <div class="volumeBar">
-            <button class="controlButton volume" title="Volume">
-              <i class="fa-solid fa-volume-high"></i> 
-            </button>
-            <div class="progressBar">
-                <div class="progressBarBg">
-                  <div class="progress"></div>
-                </div>
+            <div class="group">
+              <div class="navItem">
+                <a href="browse.php" class="navItemLink">Browse</a>
               </div>
-          </div>
-        </div>
+              <div class="navItem">
+                <a href="yourmucis.php" class="navItemLink">Your Music</a>
+              </div>
+              <div class="navItem">
+                <a href="profile.php" class="navItemLink">Profile</a>
+              </div>
+            </div>
 
+          </nav>
+        </div>
       </div>
+
+      <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBar">
+
+          <div id="nowPlayingLeft">
+            <div class="content">
+              <span class="albumLink">
+                <img class="albumArtwork" src="https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2011/11/square-format-01.jpg" alt="">
+              </span>
+              <div class="trackInfo">
+                <span class="trackName">
+                  <span>Happy Birthday</span>
+                </span>
+                <span class="artistName">
+                  <span>John Doe</span>
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div id="nowPlayingCenter">
+            <div class="content playerControls">
+              <div class="buttons">
+                <button class="controlButton shuffle" title="Shuffle">
+                  <i class="fa-solid fa-shuffle"></i>
+                </button>
+                <button class="controlButton previous" title="Previous">
+                  <i class="fa-solid fa-arrow-left"></i>
+                </button>
+                <button class="controlButton play" title="Play">
+                  <i class="fa-solid fa-play"></i>
+                </button>
+                <button class="controlButton pause" title="Pause" style="display:none">
+                  <i class="fa-solid fa-pause"></i>
+                </button>
+                <button class="controlButton next" title="Next">
+                  <i class="fa-solid fa-arrow-right"></i>
+                </button>
+                <button class="controlButton repeat" title="Repeat">
+                  <i class="fa-solid fa-repeat"></i>
+                </button>
+              </div>
+
+              <div class="playbackBar">
+                <span class="progressTime current">0:00</span>
+                <div class="progressBar">
+                  <div class="progressBarBg">
+                    <div class="progress"></div>
+                  </div>
+                </div>
+                <span class="progressTime remaining">0:00</span>
+              </div>
+            </div>
+          </div>
+
+          <div id="nowPlayingRight">
+            <div class="volumeBar">
+              <button class="controlButton volume" title="Volume">
+                <i class="fa-solid fa-volume-high"></i> 
+              </button>
+              <div class="progressBar">
+                  <div class="progressBarBg">
+                    <div class="progress"></div>
+                  </div>
+                </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
     </div>
 
   </body>
