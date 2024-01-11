c28e57adc21340b1024179b79bc0b31eaad7e557
Nikita <nikita.h@visualcomposer.com>
2024-01-11 21:41:14 +0200

    62 Lesson, Add main view containers

--- index.php
+++ index.php
@@ -27,6 +27,12 @@
 
       <div id="topContainer">
         <?php include("includes/navBarContainer.php"); ?>
+
+        <div id="mainViewContainer">
+          <div id="mainContent">
+            
+          </div>
+        </div>
       </div>
 
       <?php include("includes/nowPlayingBar.php"); ?>
