9950644fa4ddf79f6d200d83fb20a918bc7b96fe
Nikita <nikita.h@visualcomposer.com>
2024-01-14 22:02:58 +0200

    76 Lesson, Crete Song class

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
   include("includes/config.php");
   include("includes/classes/Artist.php");
   include("includes/classes/Album.php");
+  include("includes/classes/Song.php");
 
   // Manually log out
   // session_destroy();
