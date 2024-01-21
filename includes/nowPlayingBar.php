da77e677df493b7dd30b9fbf02ef2332b8b165a0
Nikita <nikita.h@visualcomposer.com>
2024-01-21 22:30:13 +0200

    Add volume bar functionality

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -15,6 +15,11 @@
     currentPlaylist = <?php echo $jsonArray ?>;
     audioElement = new Audio();
     setTrack(currentPlaylist[0], currentPlaylist, false);
+    updateVolumeProgressBar(audioElement.audio);
+
+    $('#nowPlayingBarContainer').on('mousedown touchstart mousemove touchmove', function(e) {
+      e.preventDefault();
+    });
 
     $('.playbackBar .progressBar').mousedown(function() {
       mouseDown = true;
@@ -28,6 +33,28 @@
 
     $('.playbackBar .progressBar').mouseup(function(e) {
       timeFromOffset(e, this);
+    });
+
+    $('.volumeBar .progressBar').mousedown(function() {
+      mouseDown = true;
+    });
+
+    $('.volumeBar .progressBar').mousemove(function(e) {
+      if (mouseDown) {
+        var percentage = e.offsetX / $(this).width();
+
+        if (percentage >= 0 && percentage <=1) {
+          audioElement.audio.volume = percentage;
+        }
+      }
+    });
+
+    $('.volumeBar .progressBar').mouseup(function(e) {
+      var percentage = e.offsetX / $(this).width();
+
+      if (percentage >= 0 && percentage <=1) {
+        audioElement.audio.volume = percentage;
+      }
     });
 
     $(document).mouseup(function() {
