02dedadc494d773ddea3c46cd4562787d7bf8ab5
Nikita <nikita.h@visualcomposer.com>
2024-02-08 12:19:40 +0200

    Section 14, Create your music page

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -3,9 +3,18 @@
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
   // page requested via ajax
   include("includes/config.php");
+  include("includes/classes/User.php");
   include("includes/classes/Artist.php");
   include("includes/classes/Album.php");
   include("includes/classes/Song.php");
+  include("includes/classes/Playlist.php");
+
+  if (isset($_GET['userLoggedIn'])) {
+    $userLoggedIn = new User($con, $_GET['userLoggedIn']);
+  } else {
+    echo "The `username` variable was not passed into page. Check the openPage JS function.";
+    exit();
+  }
 } else {
   // page manually loaded by user
   include("includes/header.php");
